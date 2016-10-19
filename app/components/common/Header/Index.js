0941d874ab6f4af119b882dda27f47e8fbc0e64f
yanweiqing <ywq1991@gmail.com>
2016-10-19 18:08:46 +0800

    mocker app init

--- app/components/common/Header/Index.js
+++ app/components/common/Header/Index.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { notification, Icon, Menu, Dropdown } from 'antd';
+import { Link } from 'react-router';
+import './Index.scss';
+
+const openNotification = () => {
+  notification.open({
+    message: '任何问题找严伟庆',
+    icon: <Icon type="smile-circle" style={{ color: '#2db7f5' }} />
+  });
+}
+
+const menu = (
+  <Menu>
+    <Menu.Item><Link to="/docs/readme">文档</Link></Menu.Item>
+    <Menu.Item><Link to="/docs/developr_log">开发者日志</Link></Menu.Item>
+  </Menu>
+);
 
 const Header = (props) => {
-    return (
-        <header>{props.system && props.system.name}</header>
-    );
+  return (
+    <header className="navbar"> 
+      <div className="container">
+        <a className="pull-left navbar-brand" href="/">
+          <h1>{props.system && props.system.name}</h1>
+        </a>
+        <ul className="pull-right navbar-nav">
+          <li><a onClick={openNotification}>反馈</a></li>
+          <li>
+            <Dropdown overlay={menu}><Link to="/">你好</Link></Dropdown>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- app/pages/index/Index.js
+++ app/pages/index/Index.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Router, Route, Redirect, Link, hashHistory } from 'react-router';
 import Layout from 'app/core/Layout';
+import RedBase from 'app/core/Layout/RedBase';
+import reducers from './reducers';
 
-@Layout.init
-export default class Index extends Layout {
-    renderMain() {
-        return <div>首页</div>
-    }
+import Test from './containers/Test';
+
+class Lay extends Layout {
+  renderContent() {
+    return <div>{this.props.children}</div>;
+  }
 }
+
+const Index = (props) => (
+  <Router history={hashHistory}>
+    <Route component={Lay}>
+      <Route path="/" component={Test} />
+      <Route path="/project/:id" component={Test} />
+      <Route path="/version/:id" component={Test} />
+      <Route path="/docs/:file" component={Test} />
+      <Redirect from="*" to="/" />
+    </Route>
+  </Router>
+);
+
+RedBase(Index, {
+  reducers
+});
